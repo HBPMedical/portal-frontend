729822f98187f1e2a5edd56d655afeb1d924c083
Nathalie Casati <nathalie.casati@chuv.ch>
2021-07-16 08:56:55 +0000

    changes for local mip proxy autoswitch

--- docker/runner/run.sh
+++ docker/runner/run.sh
@@ -3,4 +3,4 @@ set -e
 
 OPTS="-template /portal/conf/config.json.tmpl:/usr/share/caddy/html/static/config.json"
 
-eval "exec dockerize $OPTS caddy run --config /etc/caddy/Caddyfile --adapter caddyfile"
+eval "exec dockerize $OPTS caddy run --config /etc/caddy/Caddyfile --adapter caddyfile" # --environ"
