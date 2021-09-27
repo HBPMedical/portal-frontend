789729048901f91b4bc34d847726a990a0026c85
Manuel Spuhler <manuel.spuhler@gmail.com>
2021-09-27 15:24:03 +0000

    Storybook draft

--- src/components/ExperimentAnalysis/Content.tsx
+++ src/components/ExperimentAnalysis/Content.tsx
@@ -5,8 +5,8 @@ import { APIMining } from '../API';
 import { VariableEntity } from '../API/Core';
 import { ModelResponse } from '../API/Model';
 import { Alert } from '../UI/Alert';
-import ResultsErrorBoundary from '../UI/ResultsErrorBoundary';
-import Table from './Table';
+import Table, { ITable } from '../UI/Visualization2/Table';
+import { Tab, Tabs } from 'react-bootstrap';
 
 interface Props {
   apiMining?: APIMining;
@@ -15,6 +15,44 @@ interface Props {
   lookup: (code: string, pathologyCode: string | undefined) => VariableEntity;
   children: any;
 }
+
+const schema = {
+  fields: [
+    {
+      type: 'string',
+      name: ''
+    },
+    {
+      type: 'string',
+      name: 'DESD-synthdata'
+    },
+    {
+      type: 'string',
+      name: 'EDSD'
+    },
+    {
+      type: 'string',
+      name: 'PPMI'
+    }
+  ]
+};
+
+const table: ITable = {
+  profile: 'tabular-data-resource',
+  name: 'left-anterior-cingulate-gyrus',
+  data: [
+    ['Left anterior cingulate gyrus', 714, 474, 1000],
+    ['Datapoints', 714, 437, 920],
+    ['Nulls', 0, 37, 80],
+    ['std', 0.56, 0.696, 0.7897],
+    ['max', 6.71, 6.534, 6.534],
+    ['min', 3.16, 0.001, 0.001],
+    ['mean', 4.687, 4.45, 4.44]
+  ],
+  schema: schema
+};
+
+const tables = [table, table, table];
 
 const Content = ({
   apiMining,
@@ -37,22 +75,32 @@ const Content = ({
           {children}
           <Card>
             <Card.Body>
-              {(!selectedDatasets || selectedDatasets.length === 0) && (
-                <p>Select some data to analyse</p>
-              )}
-
-              {selectedDatasets &&
-                selectedDatasets.length > 0 &&
-                model?.query.variables && (
-                  <ResultsErrorBoundary>
-                    <Table
-                      summaryStatistics={apiMining.state.summaryStatistics}
-                      selectedDatasets={selectedDatasets}
-                      query={model && model.query}
-                      lookup={lookup}
-                    />
-                  </ResultsErrorBoundary>
-                )}
+              <Tabs defaultActiveKey={1} id="uncontrolled-mining-tab">
+                <Tab eventKey={'1'} title="Variables">
+                  <p style={{ marginBottom: '8px' }}>
+                    Descriptive statistics for the variables of interest.
+                  </p>
+                  <>
+                    {(!selectedDatasets || selectedDatasets.length === 0) && (
+                      <p>Select some data to analyse</p>
+                    )}
+                    {tables &&
+                      tables.map((table, i) => (
+                        <Table key={`table-${i}`} table={table} layout="statistics" />
+                      ))}
+                  </>
+                </Tab>
+                <Tab eventKey={'2'} title="Model">
+                  <p style={{ marginBottom: '8px' }}>
+                    Intersection table for the variables of interest as it
+                    appears in the experiment.
+                  </p>
+                  {tables &&
+                    tables.map((table, i) => (
+                      <Table key={`table-${i}`} table={table} layout="statistics" />
+                    ))}
+                </Tab>
+              </Tabs>
             </Card.Body>
           </Card>
         </Card.Body>
---
+++ src/components/UI/Visualization2/Table.tsx
@@ -0,0 +1,107 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+export interface ITable {
+  profile: 'tabular-data-resource';
+  name: string;
+  data: (string | number)[][];
+  schema: {
+    fields: {
+      type: string;
+      name: string;
+    }[];
+  };
+}
+
+interface TableProps {
+  table: ITable;
+  layout?: 'default' | 'statistics';
+}
+
+interface LayoutProps {
+  layout: 'default' | 'statistics';
+}
+
+const DataTable = styled.table<LayoutProps>`
+  font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  margin-bottom: 32px;
+  table-layout: fixed;
+  white-space: nowrap;
+  min-width: 100%;
+  border-collapse: collapse;
+  box-shadow: 0 0 0 1px #e3e3e3;
+  border-radius: 2px;
+  border: 1px solid #eee;
+
+  tr {
+    height: 24px;
+  }
+
+  th {
+    background: #ebebeb;
+    padding: 1px 4px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-weight: bold;
+    text-align: center;
+    border: 1px solid #e3e3e3;
+    border-bottom: 1px solid #bbb;
+  }
+
+  th:first-child {
+    border-left: 1px solid #eee;
+    width: 200px !important;
+    text-align: left;
+  }
+
+  ${(prop): string =>
+    prop.layout === 'default'
+      ? `
+        tr:nth-child(even) {
+          background: #ebebeb;
+          padding: 8px;
+        }
+      `
+      : `
+      tr:nth-child(1), 
+      tr:nth-child(3), 
+      tr:last-child {
+        border-bottom: 2px solid #1e1e1e;
+      }
+    `}
+
+  td {
+    border: 1px solid #e3e3e3;
+    padding: 1px 4px;
+    text-overflow: ellipsis;
+    text-align: center;
+  }
+
+  td:first-child {
+    font-weight: bold;
+    text-align: left;
+  }
+`;
+
+const Table = ({ table, layout = 'default' }: TableProps): JSX.Element => (
+  <DataTable layout={layout} key={`table-${table.name}`}>
+    <thead>
+      <tr>
+        {table.schema.fields.map((field, i) => (
+          <th key={`${table.name}-header-${i}`}>{field.name}</th>
+        ))}
+      </tr>
+    </thead>
+    <tbody>
+      {table.data.map((row, i) => (
+        <tr key={`${table.name}-row-${i}`}>
+          {row.map((data, j) => (
+            <td key={`${table.name}-col-${i}-${j}`}>{data}</td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  </DataTable>
+);
+
+export default Table;
