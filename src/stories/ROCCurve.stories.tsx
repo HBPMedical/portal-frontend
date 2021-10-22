453ccea88aa78f5718e08e29226036f0748b6101
molly <katsouli.mo@gmail.com>
2021-10-22 12:34:45 +0300

    Adds ROC curve prototype

---
+++ src/stories/ROCCurve.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import ROCCurve from '../components/UI/Visualization2/ROCCurve';
+
+export default {
+  title: 'Charts/ROCCurve',
+  component: ROCCurve
+} as ComponentMeta<typeof ROCCurve>;
+
+const Template: ComponentStory<typeof ROCCurve> = args => <ROCCurve />;
+
+export const Default = Template.bind({});
+Default.args = { layout: 'default' };
+
+export const Statistics = Template.bind({});
+Statistics.args = { layout: 'statistics' };
