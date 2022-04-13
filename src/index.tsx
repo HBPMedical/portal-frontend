b722deea80003c790b36c7f36f2fa39fbcb9c8c5
Steve Reis <stevereis93@gmail.com>
2022-04-13 18:51:28 +0200

    wip: integration matomo

--- src/index.tsx
+++ src/index.tsx
@@ -9,11 +9,23 @@ import { apolloClient } from './components/API/GraphQL/apollo.config';
 import { default as AppContainer } from './components/App/Container';
 import './index.css';
 import { unregister } from './registerServiceWorker';
+import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react';
 
 const commonApp = (): JSX.Element => {
+  const matomoInstance = createInstance({
+    urlBase: `${window.location.protocol}//stats.humanbrainproject.eu/`,
+    siteId: 29,
+    linkTracking: false,
+    configurations: {
+      disableCookies: true
+    },
+    disabled: process.env.NODE_ENV !== 'production'
+  });
   return (
     <ApolloProvider client={apolloClient}>
-      <AppContainer />
+      <MatomoProvider value={matomoInstance}>
+        <AppContainer />
+      </MatomoProvider>
     </ApolloProvider>
   );
 };
