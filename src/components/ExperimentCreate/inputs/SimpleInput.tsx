b093ac33842205c451b7d28963d369afee82c055
Steve Reis <stevereis93@gmail.com>
2022-05-09 17:17:10 +0200

    wip: improve helper

--- src/components/ExperimentCreate/inputs/SimpleInput.tsx
+++ src/components/ExperimentCreate/inputs/SimpleInput.tsx
@@ -21,7 +21,10 @@ const SimpleInput = ({ parameter, handleValueChanged }: Props) => {
     defaultValue,
     placeholder: defaultValue
   };
+  const helpLine = [];
   const helper = [parameter.hint];
+
+  if (parameter.isRequired) helpLine.push('Required');
 
   if (parameter.__typename === 'StringParameter') {
     options.type = 'text';
@@ -32,15 +35,11 @@ const SimpleInput = ({ parameter, handleValueChanged }: Props) => {
     options.max = parameter.max ?? undefined;
     options.step = parameter.isReal ? 0.01 : 1;
     options.type = 'number';
-    if (parameter.min || parameter.max)
-      helper.push(
-        `${parameter.min ? `min value: ${parameter.min} ` : ''}${
-          parameter.max ? `max value: ${parameter.max}` : ''
-        }`
-      );
+    if (parameter.min) helpLine.push(`min value: ${parameter.min}`);
+    if (parameter.max) helpLine.push(`max value: ${parameter.max}`);
   }
 
-  if (parameter.isRequired) helper.push('Required');
+  if (helpLine.length > 0) helper.push(helpLine.join(', '));
 
   return (
     <Form.Group>
@@ -49,11 +48,14 @@ const SimpleInput = ({ parameter, handleValueChanged }: Props) => {
         {...options}
         onChange={e => handleValueChanged?.(parameter.name, e.target.value)}
       ></Form.Control>
-      {helper.map((text, i) => (
-        <Form.Text key={i} className="text-muted">
-          {text}
-        </Form.Text>
-      ))}
+      {helper
+        .filter(t => t && t?.length > 1)
+        .map(t => t as string)
+        .map((text, i) => (
+          <Form.Text key={i} className="text-muted">
+            {text?.charAt(0).toUpperCase() + text?.slice(1)}
+          </Form.Text>
+        ))}
     </Form.Group>
   );
 };
