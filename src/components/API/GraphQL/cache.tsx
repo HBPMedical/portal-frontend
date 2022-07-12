af8af3ced8d4cb9aba2b6d166e86883065e990ed
Steve Reis <stevereis93@gmail.com>
2022-07-12 11:19:19 +0200

    fix: Cache policy on configuration causing issue

--- src/components/API/GraphQL/cache.tsx
+++ src/components/API/GraphQL/cache.tsx
@@ -57,9 +57,6 @@ export const cacheConfig = {
     BaseParameter: ['StringParameter', 'NominalParameter', 'NumberParameter']
   },
   typePolicies: {
-    Configuration: {
-      keyFields: ['connectorId']
-    },
     //Group, Variable and Dataset are weak entities, closely related to a domain (pathology)
     //We need to disable the cache system for them to avoid conflicts
     Group: {
