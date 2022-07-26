c6689512af817a390ed5b999ddc15367bd36bd94
Steve Reis <stevereis93@gmail.com>
2022-07-26 17:31:47 +0200

    refactor: Clean unused dependencies ci: Test issue after alogrithm model change

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,14 +1,5 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import 'jest-canvas-mock';
 import '@testing-library/jest-dom'
-
-function noOp() {}
-
-if (typeof window.URL.createObjectURL === 'undefined') {
-  Object.defineProperty(window.URL, 'createObjectURL', { value: noOp });
-}
+import '@testing-library/jest-dom/extend-expect';
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 5 * 60 * 1000;
-
-configure({ adapter: new Adapter() });
