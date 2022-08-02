88ee204d9a7b3aa413f78c05a856a5e8c4f694e5
Steve Reis <stevereis93@gmail.com>
2022-08-02 18:03:47 +0200

    wip: Refresh token

--- src/components/API/GraphQL/apollo.config.tsx
+++ src/components/API/GraphQL/apollo.config.tsx
@@ -1,39 +1,11 @@
-import { ApolloClient, ApolloLink, HttpLink } from '@apollo/client';
-import { onError } from '@apollo/client/link/error';
-import { SessionState } from '../../../utilities/types';
+import { ApolloClient, from, HttpLink } from '@apollo/client';
 import config from '../RequestHeaders';
 import { graphQLURL } from '../RequestURLS';
-import { cache, sessionStateVar } from './cache';
-
-const excludedPaths = ['/login', '/access', '/tos'];
-const excludedDomains = ['login', 'logout'];
-
-const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
-  if (
-    excludedDomains.includes(operation?.operationName) ||
-    excludedPaths.includes(window?.location?.pathname)
-  )
-    return;
-  if (graphQLErrors)
-    graphQLErrors.forEach(({ message, path, extensions }) => {
-      switch (extensions?.status || -1) {
-        case 401:
-          if (sessionStateVar() !== SessionState.INVALID)
-            sessionStateVar(SessionState.INVALID);
-          break;
-        case 403:
-          if (sessionStateVar() !== SessionState.ACCESS_DENIED)
-            sessionStateVar(SessionState.ACCESS_DENIED);
-          break;
-      }
-
-      console.log(`[GraphQL error]: Message: ${message}, Path: ${path}`);
-    });
-  if (networkError) console.log(`[Network error]: ${networkError}`);
-});
+import { cache } from './cache';
+import errorLink from './links/errorLink';
 
 export const apolloClient = new ApolloClient({
-  link: ApolloLink.from([
+  link: from([
     errorLink,
     new HttpLink({
       uri: graphQLURL,
