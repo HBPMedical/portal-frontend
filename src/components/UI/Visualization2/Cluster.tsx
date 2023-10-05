f483ccbd7194a5510171e39f4fda02e236a77703
Manuel Spuhler <manuel.spuhler@chuv.ch>
2023-10-05 14:48:56 +0200

    feat: kmeans 3d

--- src/components/UI/Visualization2/Cluster.tsx
+++ src/components/UI/Visualization2/Cluster.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { ClusterResult } from '../../API/GraphQL/types.generated';
+import Plot from 'react-plotly.js';
 
 const Container = styled.div`
   align-self: center;
@@ -17,25 +18,54 @@ interface Props {
 
 const Cluster = ({ ...props }: Props) => {
   const containerRef = useRef<HTMLDivElement>(null);
-  const Bokeh = window.Bokeh;
-  const plot = Bokeh.Plotting;
   const slug = 'cluster-kmeans';
-
   const { data } = props;
-
-  const tools = 'pan,crosshair,wheel_zoom,box_zoom,reset,save';
-  const p = plot.figure({ title: data.name, tools: tools });
-
-  p.circle(...data.nmatrix, { size: 50 });
 
   useEffect(() => {
     if (containerRef.current) containerRef.current.innerHTML = '';
-    plot.show(p, `#${slug}`);
-  }, [plot, props.data]);
+    if (data.nmatrix.length === 2) {
+      const Bokeh = window.Bokeh;
+      const plot = Bokeh.Plotting;
+      const tools = 'pan,crosshair,wheel_zoom,box_zoom,reset,save';
+      const p = plot.figure({ title: data.name, tools: tools });
+      p.circle(...data.nmatrix, { size: 50 });
+      plot.show(p, `#${slug}`);
+    }
+  }, [props.data]);
 
   if (data.nmatrix.length === 2)
     return <Container id={slug} className="result" ref={containerRef} />;
-  else
+  else if (data.nmatrix.length === 3) {
+    return (
+      <Plot
+        data={[
+          {
+            x: data.nmatrix[0],
+            y: data.nmatrix[1],
+            z: data.nmatrix[2],
+            mode: 'markers',
+            marker: {
+              size: 12,
+              line: {
+                color: 'rgba(217, 217, 217, 0.14)',
+                width: 0.5,
+              },
+              opacity: 0.8,
+            },
+            type: 'scatter3d',
+          },
+        ]}
+        layout={{
+          margin: {
+            l: 0,
+            r: 0,
+            b: 0,
+            t: 0,
+          },
+        }}
+      />
+    );
+  } else
     return (
       <div>
         No plotting available, as it&apos;s a {data.nmatrix[0].length}{' '}
